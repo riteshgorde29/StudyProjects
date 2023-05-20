3f56759f62da0b38b1725eb0df3d514730aac715
ritesh <riteshgorde29@gmail.com>
2023-05-20 10:47:26 +0530

    study project

---
+++ login aws.js
@@ -0,0 +1,24 @@
+//simple  login page that asks user id & password
+function login() { 
+    var id = prompt("Enter your ID");
+    var password = prompt("Enter your password");
+    if (id == "admin" && password == "1234") {
+        alert("Welcome to the admin page");
+    }
+    else {
+        alert("Wrong ID or password");
+    }
+}    
+
+//create first name last name login page
+
+function login() { 
+    var id = prompt("Enter your ID");
+    var password = prompt("Enter your password");
+    if (id == "admin" && password == "1234") {
+        alert("Welcome to the admin page");
+    }
+    else {
+        alert("Wrong ID or password");
+    }
+}
